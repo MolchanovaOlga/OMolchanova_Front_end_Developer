06f6d43ea33dfe6a66841f20af9fae19fc6065eb
MolchanovaOlga <designer.molchanova@gmail.com>
2024-02-18 18:11:00 +0200

    Added id to link of pdf file

--- src/main.js
+++ src/main.js
@@ -1,2 +1,6 @@
 import './js/load-page.js';
 import './js/menu.js';
+
+import pdfUrl from './public/files/resume.pdf'
+document.getElementById('link-pdf').href = pdfUrl;
+console.log(document.getElementById('link-pdf'))
