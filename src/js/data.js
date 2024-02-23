a418a82fa80c7e88d82ad736e6ef687fa3d13e2f
MolchanovaOlga <designer.molchanova@gmail.com>
2024-02-23 14:16:28 +0200

    Added lists of projects to portfolio page

--- src/js/data.js
+++ src/js/data.js
@@ -1,67 +1,88 @@
 const teamProject = [
-    {
-        name: "Focuse Frame",
-        technologies: "HTML, CSS, JS, Vite",
-        descriptionUA: "Веб-сайт розроблений для школи фотографії з адаптивним дизайном, бургер-меню, модальним вікном, формами та анімаціями.",
-        descriptionEN: "The website is created for photography school with responsive design, burger menu, modal window, forms and animations.",
-        projectEN: "Team project",
-        projectUA: "Командний проєкт",
-        urlOfSite: "https://maxdemo1.github.io/project_First_Flight/",
-        urlOfGithub: "https://github.com/maxdemo1/project_First_Flight",
-        imageBig: "",
-        imageSmall: "",
-    },
-    {
-        name: "Energy Flow",
-        technologies: "HTML, CSS, JS, Vite",
-        descriptionUA: "Веб-сайт розроблений для бажаючих мати гарну фізичну форму з адаптивним дизайном, мотивуючою цитатою дня, пошуком за ключовим словом, можливістю додати вправу до улюблених, модальними вікнами, формами та пагінацією.",
-        descriptionEN: "The website is created for fitness enthusiasts with responsive design, motivational quote of the day, keyword search, option to add exercise to favorites, modal windows, forms and pagination.",
-        projectEN: "Team project",
-        projectUA: "Командний проєкт",
-        urlOfSite: "https://karolinataran.github.io/project-dev-hunters/",
-        urlOfGithub: "https://github.com/KarolinaTaran/project-dev-hunters",
-        imageBig: "",
-        imageSmall: "",
-    },
-]
+  {
+    name: 'Focuse Frame',
+    technologies: 'HTML, CSS, JS, Vite',
+    descriptionUA:
+      'Веб-сайт розроблений для школи фотографії з адаптивним дизайном, бургер-меню, модальним вікном, формами та анімаціями.<br>Роль: розробник мобільного модального вікна.',
+    descriptionEN:
+      'The website is created for photography school with responsive design, burger menu, modal window, forms and animations.<br>Role: developer of  mobile modal.',
+    projectEN: 'Team project',
+    projectUA: 'Командний проєкт',
+    urlOfSite: 'https://maxdemo1.github.io/project_First_Flight/',
+    urlOfGithub: 'https://github.com/maxdemo1/project_First_Flight',
+    imageMobBig: './img/team-projects/focus-frame-mob-2x.jpg',
+    imageMobSmall: './img/team-projects/focus-frame-mob-1x.jpg',
+    imageTablDeskBig: './img/team-projects/focus-frame-tabl-desk-2x.jpg',
+    imageTablDeskSmall: './img/team-projects/focus-frame-tabl-desk-1x.jpg',
+  },
+  {
+    name: 'Energy Flow',
+    technologies: 'HTML, CSS, JS, Vite',
+    descriptionUA:
+      'Веб-сайт розроблений для бажаючих мати гарну фізичну форму з адаптивним дизайном, мотивуючою цитатою дня, пошуком за ключовим словом, можливістю додати вправу до улюблених, модальними вікнами, формами та пагінацією.<br>Роль: розробник модального вікна вправи та частково модального вікна рейтингу.',
+    descriptionEN:
+      'The website is created for fitness enthusiasts with responsive design, motivational quote of the day, keyword search, option to add exercise to favorites, modal windows, forms and pagination.<br>Role: developer of exercise modal and partly of rating modal.',
+    projectEN: 'Team project',
+    projectUA: 'Командний проєкт',
+    urlOfSite: 'https://karolinataran.github.io/project-dev-hunters/',
+    urlOfGithub: 'https://github.com/KarolinaTaran/project-dev-hunters',
+    imageMobBig: './img/team-projects/energy-flow-mob-2x.jpg',
+    imageMobSmall: './img/team-projects/energy-flow-mob-1x.jpg',
+    imageTablDeskBig: './img/team-projects/energy-flow-tabl-desk-2x.jpg',
+    imageTablDeskSmall: './img/team-projects/energy-flow-tabl-desk-1x.jpg',
+  },
+];
 
 const individualProject = [
-    {
-        name: "Timer",
-        technologies: "HTML, CSS, JS, Vite",
-        descriptionUA: "Застосунок розроблений для зворотного відліку часу до певної дати в майбутньому з використанням бібліотек Flatpickr та iziToast.",
-        descriptionEN: "The application is created to count down the time to a certain date in the future using the Flatpickr and iziToast libraries.",
-        projectEN: "Individual project",
-        projectUA: "Індивідуальний проєкт",
-        urlOfSite: "https://molchanovaolga.github.io/goit-js-hw-10/1-timer.html",
-        urlOfGithub: "https://github.com/MolchanovaOlga/goit-js-hw-10/blob/main/src/js/1-timer.js",
-        imageBig: "",
-        imageSmall: "",
-    },
-    {
-        name: "Gallery",
-        technologies: "HTML, CSS, JS, Vite",
-        descriptionUA: "Застосунок пошуку зображень за ключовим словом і їх перегляду в галереї з плавним прокручуванням сторінки.  Використано бібліотеки  Axios,  iziToast, css-loader, SimpleLightbox та АРІ сервіс Pixabay.",
-        descriptionEN: "The application is created for searching images by keyword and viewing them in a gallery with smooth page scrolling. The libraries Axios, iziToast, css-loader, SimpleLightbox and the API service of Pixabay were used.",
-        projectEN: "Individual project",
-        projectUA: "Індивідуальний проєкт",
-        urlOfSite: "https://molchanovaolga.github.io/goit-js-hw-12/",
-        urlOfGithub: "https://github.com/MolchanovaOlga/goit-js-hw-12",
-        imageBig: "",
-        imageSmall: "",
-    },
-    {
-        name: "WebStudio",
-        technologies: "HTML, JS, SCSS, SASS",
-        descriptionUA: "Cайт розроблений для компанії  “WebStudio”. Адаптивний дизайн, ефекти трансформації, модальні вікна.",
-        descriptionEN: "The site is created for the company `WebStudio`. Adaptive design, transformation effects, modal windows.",
-        projectEN: "Individual project",
-        projectUA: "Індивідуальний проєкт",
-        urlOfSite: "https://molchanovaolga.github.io/GoIt-markup-hw-8-uk/",
-        urlOfGithub: "https://github.com/MolchanovaOlga/GoIt-markup-hw-8-uk",
-        imageBig: "",
-        imageSmall: "",
-    },
-]
+  {
+    name: 'WebStudio',
+    technologies: 'HTML, JS, SCSS, SASS',
+    descriptionUA:
+      'Cайт розроблений для компанії  “WebStudio”. Адаптивний дизайн, ефекти трансформації, модальні вікна.',
+    descriptionEN:
+      'The site is created for the company `WebStudio`. Adaptive design, transformation effects, modal windows.',
+    projectEN: 'Individual project',
+    projectUA: 'Індивідуальний проєкт',
+    urlOfSite: 'https://molchanovaolga.github.io/GoIt-markup-hw-8-uk/',
+    urlOfGithub: 'https://github.com/MolchanovaOlga/GoIt-markup-hw-8-uk',
+    imageMobBig: './img/individual projects/web-studio-mob-2x.jpg',
+    imageMobSmall: './img/individual projects/web-studio-mob-1x.jpg',
+    imageTablDeskBig: './img/individual projects/web-studio-tabl-desk-2x.jpg',
+    imageTablDeskSmall: './img/individual projects/web-studio-tabl-desk-1x.jpg',
+  },
+  {
+    name: 'Timer',
+    technologies: 'HTML, CSS, JS, Vite',
+    descriptionUA:
+      'Застосунок розроблений для зворотного відліку часу до певної дати в майбутньому з використанням бібліотек Flatpickr та iziToast.',
+    descriptionEN:
+      'The application is created to count down the time to a certain date in the future using the Flatpickr and iziToast libraries.',
+    projectEN: 'Individual project',
+    projectUA: 'Індивідуальний проєкт',
+    urlOfSite: 'https://molchanovaolga.github.io/goit-js-hw-10/1-timer.html',
+    urlOfGithub:
+      'https://github.com/MolchanovaOlga/goit-js-hw-10/blob/main/src/js/1-timer.js',
+    imageMobBig: './img/individual projects/timer-mob-2x.jpg',
+    imageMobSmall: './img/individual projects/timer-mob-1x.jpg',
+    imageTablDeskBig: './img/individual projects/timer-tabl-desk-2x.jpg',
+    imageTablDeskSmall: './img/individual projects/timer-tabl-desk-1x.jpg',
+  },
+  {
+    name: 'Gallery',
+    technologies: 'HTML, CSS, JS, Vite',
+    descriptionUA:
+      'Застосунок пошуку зображень за ключовим словом і їх перегляду в галереї з плавним прокручуванням сторінки.  Використано бібліотеки  Axios,  iziToast, css-loader, SimpleLightbox та АРІ сервіс Pixabay.',
+    descriptionEN:
+      'The application is created for searching images by keyword and viewing them in a gallery with smooth page scrolling. The libraries Axios, iziToast, css-loader, SimpleLightbox and the API service of Pixabay were used.',
+    projectEN: 'Individual project',
+    projectUA: 'Індивідуальний проєкт',
+    urlOfSite: 'https://molchanovaolga.github.io/goit-js-hw-12/',
+    urlOfGithub: 'https://github.com/MolchanovaOlga/goit-js-hw-12',
+    imageMobBig: './img/individual projects/gallery-mob-2x.jpg',
+    imageMobSmall: './img/individual projects/gallery-mob-1x.jpg',
+    imageTablDeskBig: './img/individual projects/gallery-tabl-desk-2x.jpg',
+    imageTablDeskSmall: './img/individual projects/gallery-tabl-desk-1x.jpg',
+  },
+];
 
 export { teamProject, individualProject };
--- src/js/portfolio.js
+++ src/js/portfolio.js
@@ -1,12 +1,95 @@
 import { teamProject, individualProject } from './data';
 
-const form = document.querySelector('.portfolio-radio-form');
+//const form = document.querySelector('.portfolio-radio-form');
 const teamProjectBtn = document.querySelector('input[value="team-projects"]');
-const individualProjectBtn = document.querySelector('input[value="individual-projects"]');
+const individualProjectBtn = document.querySelector(
+  'input[value="individual-projects"]'
+);
+const projectList = document.querySelector('.portfolio-projects-list');
 
-// teamProjectBtn.addEventListener('click', loadTeamProjects);
-// individualProjectBtn.addEventListener('click', loadIndividualProjects);
+//teamProjectBtn.addEventListener('click', loadTeamProjects);
+//individualProjectBtn.addEventListener('click', loadIndividualProjects);
 
-// function loadTeamProjects() {
+function loadTeamProjects() {
+  toggleRadioBtns();
+  projectList.innerHTML = '';
+  drawProjectsList(teamProject);
+}
 
-// }
+function loadIndividualProjects() {
+  toggleRadioBtns();
+  projectList.innerHTML = '';
+  drawProjectsList(individualProject);
+}
+
+function drawProjectsList(arr) {
+  const projectsItem = arr
+    .reverse()
+    .map(
+      ({
+        name,
+        technologies,
+        descriptionUA,
+        projectUA,
+        urlOfSite,
+        urlOfGithub,
+        imageMobBig,
+        imageMobSmall,
+        imageTablDeskBig,
+        imageTablDeskSmall,
+      }) => {
+        return `
+    <li class="portfolio-projects-item">
+    <div class="portfolio-projects-left-block">
+      <a class="portfolio-projects-site-link" href="${urlOfSite}" target="_blank">
+      <div class="portfolio-projects-image-container">
+        <picture>
+          <source
+          media="(max-width: 767px)"
+          srcset="
+          ${imageMobSmall} 1x,
+          ${imageMobBig} 2x
+          "
+          />
+          <source
+            media="(min-width: 768px)"
+            srcset="
+            ${imageTablDeskSmall} 1x,
+            ${imageTablDeskBig} 2x
+            "
+          />
+          <img class="portfolio-projects-image" src="${imageTablDeskSmall}" alt="${name} project" />
+        </picture>
+        <div class="image-blur">
+        <p class="image-blur-text">Перейти на сайт</p>
+        </div>
+        </div>
+      </a>
+      <div class="projects-left-text-container">
+        <a class="portfolio-projects-github-link" href="${urlOfGithub}" target="_blank">
+          <svg class="portfolio-projects-github-icon" width="24" height="24">
+            <use href="./img/sprite.svg#icon-github"></use>
+          </svg>
+        Переглянути код
+        </a>
+        <p class="portfolio-projects-filter-paragraf">${projectUA}</p>
+      </div>
+    </div>
+    <div class="portfolio-projects-right-block">
+    <h3>${name}</h3>
+    <p>${technologies}</p>
+    <p>${descriptionUA}</p>
+    </div>
+  </li>
+    `;
+      }
+    )
+    .join('');
+
+  projectList.insertAdjacentHTML('beforeend', projectsItem);
+}
+
+function toggleRadioBtns() {
+  teamProjectBtn.parentNode.classList.toggle('selected-radio-label');
+  individualProjectBtn.parentNode.classList.toggle('selected-radio-label');
+}
