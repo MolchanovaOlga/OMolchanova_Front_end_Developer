c9b5878b90d0f5f993cfd73a26bd44315c8bdbab
MolchanovaOlga <designer.molchanova@gmail.com>
2024-02-24 22:13:35 +0200

    Changed of ways of images url to portfolio page

--- src/main.js
+++ src/main.js
@@ -6,3 +6,43 @@ import './js/portfolio.js';
 
 import pdfUrl from './public/files/Olga_Molchanova_FullStack_Developer.pdf';
 document.getElementById('link-pdf').href = pdfUrl;
+
+// --------------------- urls of jpg-file to data.js ---------------------
+
+import { teamProject, individualProject } from './js/data.js';
+import { teamImgsUrl, individualImgsUrl } from './js/dataOfImgsUrl.js';
+
+teamProject.forEach(obj => {
+  teamImgsUrl.forEach(
+    ({
+      imageMobBigUrl,
+      imageMobSmallUrl,
+      imageTablDeskBigUrl,
+      imageTablDeskSmallUrl,
+    }) => {
+      obj.imageMobBig = `./public${imageMobBigUrl}`;
+      obj.imageMobSmall = `./public${imageMobSmallUrl}`;
+      obj.imageTablDeskBig = `./public${imageTablDeskBigUrl}`;
+      obj.imageTablDeskSmall = `./public${imageTablDeskSmallUrl}`;
+    }
+  );
+});
+
+individualProject.forEach(obj => {
+  individualImgsUrl.forEach(
+    ({
+      imageMobBigUrl,
+      imageMobSmallUrl,
+      imageTablDeskBigUrl,
+      imageTablDeskSmallUrl,
+    }) => {
+      obj.imageMobBig = `./public${imageMobBigUrl}`;
+      obj.imageMobSmall = `./public${imageMobSmallUrl}`;
+      obj.imageTablDeskBig = `./public${imageTablDeskBigUrl}`;
+      obj.imageTablDeskSmall = `./public${imageTablDeskSmallUrl}`;
+    }
+  );
+});
+
+console.log(teamProject);
+console.log(individualProject);
